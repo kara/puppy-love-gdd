ec257a9821f015f34980ffcce44782151222bcd4
Kara Erickson <karakara@google.com>
2016-12-08 11:33:44 +0800

    commit from gdd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation, trigger, transition, style, animate, state} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  animations: [
+    trigger('fadeIn', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [
+        style({ opacity: 0}),
+        animate('750ms ease-out')
+      ])
+    ])
+  ]
 })
 export class AppComponent {
   title = 'app works!';
+
+
+  dogs = [
+    {name: 'Mal', img: 'Mal.png'},
+    {name: 'Porter', img: 'Porter.png'},
+    {name: 'Koby', img: 'Koby.png'},
+    {name: 'Razzle', img: 'Razzle.png'},
+    {name: 'Molly', img: 'Molly.png'},
+    {name: 'Husi', img: 'Husi.png'},
+  ];
+
 }
